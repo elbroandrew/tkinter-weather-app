1d31e5ec8ab55378e953499a560cfeabf21ebfbc
andrew <elbroandrew@gmail.com>
2022-01-22 20:37:15 +1000

    fix: config ini key

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from tkinter import *
 from configparser import ConfigParser
 import requests
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
 
 config_file = 'config.ini'
 config = ConfigParser()
 config.read(config_file)
 api_key = config['api_key']['key']
+
 
 def get_weather(city: str):
     result = requests.get(url.format(city, api_key))
@@ -15,6 +16,9 @@ def get_weather(city: str):
         print(result.content)
     else:
         return None
+
+
+get_weather("London")
 
 app = Tk()
 app.title("Weather app")
@@ -24,8 +28,9 @@ app.geometry('700x350')
 def search():
     pass
 
+
 # entry field
-city_text = StringVar() # object to set/get string
+city_text = StringVar()  # object to set/get string
 city_entry = Entry(app, textvariable=city_text)
 city_entry.pack()
 
@@ -44,12 +49,7 @@ image.pack()
 temp_lbl = Label(app, text="")
 temp_lbl.pack()
 
-
 weather_lbl = Label(app, text="")
 weather_lbl.pack()
 
-
-
-
-if __name__ == '__main__':
-    app.mainloop()
+app.mainloop()
