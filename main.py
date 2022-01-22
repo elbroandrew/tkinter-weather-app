a7f2686545d677e97b3d8cc302068fab9a45a5a2
andrew <elbroandrew@gmail.com>
2022-01-22 19:16:40 +1000

    feat: add button, image

--- main.py
+++ main.py
@@ -5,9 +5,26 @@ app = Tk()
 app.title("Weather app")
 app.geometry('700x350')
 
+
+def search():
+    pass
+
+# entry field
 city_text = StringVar() # object to set/get string
 city_entry = Entry(app, textvariable=city_text)
 city_entry.pack()
+
+# search button
+search_btn: Button = Button(app, text='Search weather', width=12, command=search)
+search_btn.pack()
+
+# label
+location_lbl = Label(app, text='Location', font=('consolas', 20))
+location_lbl.pack()
+
+# image
+image = Label(app, bitmap='')
+image.pack()
 
 
 
