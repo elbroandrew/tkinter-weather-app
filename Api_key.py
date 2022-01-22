f9589ae484a20ea5eff79d43e5d2b7409dee369c
andrew <elbroandrew@gmail.com>
2022-01-22 19:03:39 +1000

    feat: add tk

--- Api_key.py
+++ Api_key.py
@@ -1,1 +1,2 @@
-4d8722ef26a94c9cda5499124e174219
+
+API_KEY = dict(api_key="4d8722ef26a94c9cda5499124e174219")
---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from Api_key import API_KEY
+
+app = Tk()
+app.title("Weather app")
+app.geometry('700x350')
+
+
+
+
+if __name__ == '__main__':
+    app.mainloop()
